f7e7534ac46268499a89376d041c44ac5d696462
yyrrcc <music_v@naver.com>
2025-09-12 17:52:04 +0900

    new project

---
+++ src/main/java/com/mycompany/p2/SpringbootProject2250912Application.java
@@ -0,0 +1,13 @@
+package com.mycompany.p2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootProject2250912Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootProject2250912Application.class, args);
+	}
+
+}
