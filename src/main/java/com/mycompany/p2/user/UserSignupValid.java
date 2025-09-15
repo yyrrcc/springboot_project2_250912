349678931f49325782a6a0b3bfb22602d86df280
yyrrcc <music_v@naver.com>
2025-09-15 11:10:43 +0900

    null, 빈문자열, 공백문자열 모두 허용하지 않게 @NotBlank 사용

--- src/main/java/com/mycompany/p2/user/UserSignupValid.java
+++ src/main/java/com/mycompany/p2/user/UserSignupValid.java
@@ -1,6 +1,7 @@
 package com.mycompany.p2.user;
 
 import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.Size;
 import lombok.Getter;
@@ -10,24 +11,24 @@ import lombok.Setter;
 @Getter @Setter
 public class UserSignupValid {
 	
-	@NotEmpty(message = "아이디는 필수 항목입니다.")
+	@NotBlank(message = "아이디는 필수 항목입니다.")
 	@Size (min = 3, max = 20, message = "아이디는 3자 이상 20자 이하입니다.")
 	private String userid; // 아이디
 	
-	@NotEmpty(message = "비밀번호 필수 항목입니다.")
+	@NotBlank(message = "비밀번호 필수 항목입니다.")
 	@Size (min = 3, message = "비밀번호는 최소 3자 이상이어야 합니다.")
 	private String password1; // 비밀번호
 	
-	@NotEmpty(message = "비밀번호 확인은 필수 항목입니다.")
+	@NotBlank(message = "비밀번호 확인은 필수 항목입니다.")
 	private String password2; // 비밀번호 확인
 	
-	@NotEmpty(message = "이름은 필수 항목입니다.")
+	@NotBlank(message = "이름은 필수 항목입니다.")
 	private String username; // 이름
 	
 	@NotEmpty(message = "반려동물 선택은 필수 항목입니다.")
 	private String pet; // 반려동물 여부
 	
-	@NotEmpty(message = "이메일은 필수 항목입니다.")
+	@NotBlank(message = "이메일은 필수 항목입니다.")
 	@Email
 	private String email; // 이메일
 
