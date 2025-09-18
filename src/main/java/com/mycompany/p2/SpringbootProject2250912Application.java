3be51f52aefb5858ba6999d5b687bc9c64ec081b
yyrrcc <music_v@naver.com>
2025-09-18 09:19:31 +0900

    최종변경(db변경)

--- src/main/java/com/mycompany/p2/SpringbootProject2250912Application.java
+++ src/main/java/com/mycompany/p2/SpringbootProject2250912Application.java
@@ -2,12 +2,21 @@ package com.mycompany.p2;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class SpringbootProject2250912Application {
+public class SpringbootProject2250912Application extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootProject2250912Application.class, args);
 	}
+	
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(SpringbootProject2250912Application.class);
+		
+	}
+	
 
 }
